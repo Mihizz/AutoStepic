be5b8069625bd8f09c51a51de751aeb304b979cf
Mihizz <86364159+Mihizz@users.noreply.github.com>
2023-05-04 15:44:09 +0200

    Prikaz musterija

--- src/clj/autostepic/routes/home.clj
+++ src/clj/autostepic/routes/home.clj
@@ -7,16 +7,22 @@
    [ring.util.response]
    [ring.util.http-response :as response]))
 
-(defn home-page [request]
-  (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+;---------------------------------------------------------------------------------
+; --- POKRETANJE STRANICA --------------------------------------------------------
+;---------------------------------------------------------------------------------
 
-(defn about-page [request]
-  (layout/render request "about.html"))
+;POCETNA
+(defn home-page [request]
+  (layout/render request "home.html"))
+
+;------------------------------------------------
 
 (defn home-routes []
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
+
+   ;--STRANICE------
    ["/" {:get home-page}]
-   ["/about" {:get about-page}]])
+   ["/skladiste" {:get skladiste-page}]
 
