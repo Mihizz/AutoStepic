7d4af1181db16858981a43ead14600424f62bc80
Mihizz <86364159+Mihizz@users.noreply.github.com>
2023-05-04 15:48:15 +0200

    Dodavanje musterija

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,3 +1,10 @@
+-- :name create-musterija! :! :n
+-- :doc kreira novu musteriju
+INSERT INTO musterije
+(ime, prezime, email)
+VALUES (:ime, :prezime, :email)
+
+
 -- :name get-musterije :? :*
 -- :doc prikazuje listu svih musterija
 SELECT * from musterije
--- src/clj/autostepic/routes/home.clj
+++ src/clj/autostepic/routes/home.clj
@@ -17,12 +17,73 @@
 
 ;------------------------------------------------
 
+;MUSTERIJE
+(defn musterije-page [request]
+  (layout/render
+   request
+   "musterije.html"
+   {:musterije (db/get-musterije)}))
+
+;DODAJ MUSTERIJU
+(defn createMusterija-page [{:keys [flash] :as request}]
+  (layout/render
+     request
+     "create/createMusterija.html"
+     (select-keys flash [:ime :prezime :email :errors])))
+
+;---------------------------------------------------------------------------------
+; --- SHEMA ----------------------------------------------------------------------
+;---------------------------------------------------------------------------------
+
+;MUSTERIJA (INSERT)
+(def musterijaI-schema
+  	[[:ime
+  		st/required
+  		st/string]
+  	[:prezime
+  		st/required
+  		st/string]
+  	[:email
+  		st/required
+  		st/email]])
+
+;---------------------------------------------------------------------------------
+; --- VALIDACIJA -----------------------------------------------------------------
+;---------------------------------------------------------------------------------
+
+;MUSTERIJA(INSERT)
+(defn validate-musterijaI [params]
+(first (st/validate params musterijaI-schema)))
+
+;---------------------------------------------------------------------------------
+; --- METODE ---------------------------------------------------------------------
+;---------------------------------------------------------------------------------
+
+;KREIRANJE MUSTERIJE
+(defn create-musterija! [{:keys [params]}]
+  (if-let [errors (validate-musterijaI params)]
+    (-> (response/found "/dodavanjeMusterije")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+        (db/create-musterija! params)
+        (response/found "/musterije"))))
+
+;-----------------------------------------------------------------------------------------
+; --- RUTIRANJE --------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------------
+
+
 (defn home-routes []
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
 
+   ;--API---------
+   ["/dodajMusteriju" {:post create-musterija!}]
+
    ;--STRANICE------
    ["/" {:get home-page}]
-   ["/skladiste" {:get skladiste-page}]
+   ;-------
+   ["/musterije" {:get musterije-page}]
+   ["/dodavanjeMusterije" {:get createMusterija-page}]
 
